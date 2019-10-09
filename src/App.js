1a2c94fcb8ab911e0f3a41ac65b21c93eb06aa8b
Bentz <bentzjedwards@yahoo.com>
2019-10-09 13:45:49 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-33893481-5');
 
 function App() {
+
+  const onClick = () => {
+    ReactGA.event({
+      category: 'Copy change test',
+      action: 'Button clicked'
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,12 +22,13 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
+          onClick={onClick}
           className="App-link"
+          rel="noopener noreferrer"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          Click me
         </a>
       </header>
     </div>
